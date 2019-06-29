d202fc3324bc6c2514ea0d2ea29cd40599193d3e
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-06-28 23:17:50 -0400

    don't need a whole class for the parallel trainer (cherry-pick conflict)

---
+++ src/parallel-trainer.js
@@ -0,0 +1,65 @@
+import mergeNets from './utilities/merge-nets';
+import partition from './utilities/partition';
+
+/**
+ * Ensemble training, via simple parameter averaging.
+ */
+export default async function trainParallel(data, net, trainOptions) {
+  const startMs = Date.now();
+  const pOptions = trainOptions.parallel || {};
+  const threadCount = pOptions.threads || 1;
+  const minPartitionSize = Math.ceil(data.length / threadCount);
+  const partitionSize = Math.max(Math.ceil(pOptions.partitionSize || 1), minPartitionSize);
+  const partitions = partition(data, threadCount, partitionSize);
+  const NetCtor = Object.getPrototypeOf(net).constructor;
+  const maxEpochs = pOptions.maxEpochs || 1000;
+  const errorThresh = trainOptions.errorThresh || NetCtor.trainDefaults.errorThresh;
+  
+  let peerTrainOptions = Object.assign({}, trainOptions);
+  delete peerTrainOptions.parallel;
+  delete peerTrainOptions.callback;
+  delete peerTrainOptions.log;
+  
+  net.train([data[0]], {errorThresh: 0.9, iterations: 1}); // initialize weights
+  let globalWeights = net.toJSON();
+
+  let error = 1;
+  let epochs = 0;
+  let iterations = 0;
+
+  while (epochs < maxEpochs && error >= errorThresh) {
+    let promises = [];
+    let peers = [];
+
+    for (let t = 0; t < threadCount; t++) {
+      let peer = new NetCtor();
+      peer.fromJSON(globalWeights);
+      peers.push(peer);
+
+      if (pOptions.syncMode === true) {
+        promises.push(Promise.resolve(peer.train(partitions[t], peerTrainOptions)));
+      } else {
+        promises.push(peer.trainAsync(partitions[t], peerTrainOptions));
+      }
+    }
+
+    const statuses = await Promise.all(promises);
+    let worstError = 0;
+    for (let t = threadCount - 1; t >= 0; t--) {
+      const peer = peers[t];
+      const partitionIdx = (t === 0 ? threadCount : t) - 1;
+      const result = peer.test(partitions[partitionIdx][0]);
+      worstError = Math.max(result.error, worstError);
+      iterations += statuses[t].iterations;
+    }
+    error = worstError;
+    epochs++;
+
+    globalWeights = mergeNets(...peers).toJSON();
+  }
+
+  net.fromJSON(globalWeights);
+  const endMs = Date.now();
+  const elapsedMs = endMs - startMs;
+  return {error, iterations, epochs, elapsedMs};
+}
---
+++ test/base/parallel-trainer.js
@@ -0,0 +1,35 @@
+import assert from 'assert';
+import NeuralNetwork from '../../src/neural-network';
+import trainParallel from '../../src/parallel-trainer';
+
+describe('Parallel Trainer', () => {
+  describe('NeuralNetwork', () => {
+
+    const checkPerformance = (net, trainingData) => {
+      for (let i = 0; i < trainingData.length; i++) {
+        let test1 = net.test(trainingData[i]);
+        if (test1.misclasses.length > 0 || test1.error >= 0.1) {
+          console.warn(JSON.stringify(test1));
+        }
+        assert.equal(test1.total, 1);
+        assert.ok(test1.error < 0.1);
+        assert.equal(test1.misclasses.length, 0);
+      }
+    };
+
+    it('can converge via ensemble training.', async function () {
+      const trainingData = [
+        {input: [0, 1], output: [1]},
+        {input: [0, 0], output: [0]},
+        {input: [1, 0], output: [1]},
+        {input: [1, 1], output: [0]}
+      ];
+  
+      const net = new NeuralNetwork();
+      const status = await trainParallel(trainingData, net, {parallel: { threads: 2, syncMode: true }});
+      assert.ok(status.epochs > 1);
+      checkPerformance(net, trainingData);
+    });
+
+  });
+});
