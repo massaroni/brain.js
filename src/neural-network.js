7905b9aeedfdd20b00192de0e810be83875d0f71
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-06-29 13:32:42 -0400

    supporting callbacks in the log parameter for parallel training

--- src/neural-network.js
+++ src/neural-network.js
@@ -525,8 +525,10 @@ class NeuralNetwork {
     let status, endTime;
     ({ data, status, endTime } = this.prepTraining(data, options));
     
-    if (this.trainOpts.parallel) {
-      return trainParallel(rawData, this, this.getTrainOptsJSON());
+    const trainOpts = this.getTrainOptsJSON();
+    if (trainOpts.parallel) {
+      trainOpts.log = this.trainOpts.log;
+      return trainParallel(rawData, this, trainOpts);
     }
 
     return new Promise((resolve, reject) => {
