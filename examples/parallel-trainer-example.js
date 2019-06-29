0f58c150c7da93e2ec4e5633f2f20206d127a527
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-06-29 13:19:47 -0400

    parallel training example

---
+++ examples/parallel-trainer-example.js
@@ -0,0 +1,12 @@
+const brain = require('../dist/index').default;
+
+const xorTrainingData = [
+  {input: [0, 1], output: [1]},
+  {input: [0, 0], output: [0]},
+  {input: [1, 0], output: [1]},
+  {input: [1, 1], output: [0]}
+];
+
+const net = new brain.NeuralNetwork();
+
+net.trainAsync(xorTrainingData, {parallel: {threads: 3}}).then(console.log);
