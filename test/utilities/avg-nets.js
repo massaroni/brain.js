bd61c0f09e098d12b1b80cf9cdd2a25312d7d926
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-06-30 02:57:51 -0400

    should be able to average NeuralNetwork and NeuralNetworkGPU json, they should have the same model

--- test/utilities/avg-nets.js
+++ test/utilities/avg-nets.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import NeuralNetwork from '../../src/neural-network';
+import NeuralNetworkGPU from '../../src/neural-network-gpu';
 import avgNets from '../../src/utilities/avg-nets';
 
 describe('avg-nets', () => {
@@ -462,6 +463,232 @@ describe('avg-nets', () => {
       assert.deepEqual(netA.toJSON(), netA.toJSON());
     });
 
+
+  });
+
+  describe('NeuralNetworkGPU', () => {
+
+    it('averages the weights of 2 NeuralNetwork and 1 NeuralNetworkGPU', () => {
+      const jsonA = {
+        "sizes": [ 2, 2, 1 ],
+        "layers": [
+          {
+            "A": {},
+            "B": {}
+          },
+          {
+            "0": {
+              "bias": 100,
+              "weights": {
+                "A": 10,
+                "B": 1
+              }
+            },
+            "1": {
+              "bias": 10,
+              "weights": {
+                "A": 1,
+                "B": 100
+              }
+            }
+          },
+          {
+            "X": {
+              "bias": 1,
+              "weights": {
+                "0": 10,
+                "1": 1
+              }
+            }
+          }
+        ],
+        "outputLookup": true,
+        "inputLookup": true,
+        "activation": "sigmoid",
+        "trainOpts": {
+          "iterations": 20000,
+          "errorThresh": 0.005,
+          "log": false,
+          "logPeriod": 10,
+          "learningRate": 0.3,
+          "momentum": 0.1,
+          "callbackPeriod": 10,
+          "beta1": 0.9,
+          "beta2": 0.999,
+          "epsilon": 1e-8
+        }
+      };
+
+      const jsonB = {
+        "sizes": [ 2, 2, 1 ],
+        "layers": [
+          {
+            "A": {},
+            "B": {}
+          },
+          {
+            "0": {
+              "bias": 200,
+              "weights": {
+                "A": 20,
+                "B": 2
+              }
+            },
+            "1": {
+              "bias": 20,
+              "weights": {
+                "A": 2,
+                "B": 200
+              }
+            }
+          },
+          {
+            "X": {
+              "bias": 2,
+              "weights": {
+                "0": 20,
+                "1": 2
+              }
+            }
+          }
+        ],
+        "outputLookup": true,
+        "inputLookup": true,
+        "activation": "sigmoid",
+        "trainOpts": {
+          "iterations": 20000,
+          "errorThresh": 0.005,
+          "log": false,
+          "logPeriod": 10,
+          "learningRate": 0.3,
+          "momentum": 0.1,
+          "callbackPeriod": 10,
+          "beta1": 0.9,
+          "beta2": 0.999,
+          "epsilon": 1e-8
+        }
+      };
+
+      const jsonC = {
+        "sizes": [ 2, 2, 1 ],
+        "layers": [
+          {
+            "A": {},
+            "B": {}
+          },
+          {
+            "0": {
+              "bias": 600,
+              "weights": {
+                "A": 60,
+                "B": 6
+              }
+            },
+            "1": {
+              "bias": 60,
+              "weights": {
+                "A": 6,
+                "B": 600
+              }
+            }
+          },
+          {
+            "X": {
+              "bias": 6,
+              "weights": {
+                "0": 60,
+                "1": 6
+              }
+            }
+          }
+        ],
+        "outputLookup": true,
+        "inputLookup": true,
+        "activation": "sigmoid",
+        "trainOpts": {
+          "iterations": 20000,
+          "errorThresh": 0.005,
+          "log": false,
+          "logPeriod": 10,
+          "learningRate": 0.3,
+          "momentum": 0.1,
+          "callbackPeriod": 10,
+          "beta1": 0.9,
+          "beta2": 0.999,
+          "epsilon": 1e-8
+        }
+      };
+
+      const jsonExpected = {
+        "sizes": [ 2, 2, 1 ],
+        "layers": [
+          {
+            "A": {},
+            "B": {}
+          },
+          {
+            "0": {
+              "bias": 300,
+              "weights": {
+                "A": 30,
+                "B": 3
+              }
+            },
+            "1": {
+              "bias": 30,
+              "weights": {
+                "A": 3,
+                "B": 300
+              }
+            }
+          },
+          {
+            "X": {
+              "bias": 3,
+              "weights": {
+                "0": 30,
+                "1": 3
+              }
+            }
+          }
+        ],
+        "outputLookup": true,
+        "inputLookup": true,
+        "activation": "sigmoid",
+        "trainOpts": {
+          "iterations": 20000,
+          "errorThresh": 0.005,
+          "log": false,
+          "logPeriod": 10,
+          "learningRate": 0.3,
+          "momentum": 0.1,
+          "callbackPeriod": 10,
+          "beta1": 0.9,
+          "beta2": 0.999,
+          "epsilon": 1e-8
+        }
+      };
+
+      const a = new NeuralNetworkGPU();
+      a.fromJSON(jsonA);
+
+      const b = new NeuralNetwork();
+      b.fromJSON(jsonB);
+
+      const c = new NeuralNetwork();
+      c.fromJSON(jsonC);
+
+      const actual = avgNets(a, b, c);
+      const jsonActual = actual.toJSON();
+
+      assert.deepEqual(jsonExpected, jsonActual);
+      assert.equal(Object.getPrototypeOf(a), Object.getPrototypeOf(actual));
+      assert.notEqual(Object.getPrototypeOf(b), Object.getPrototypeOf(actual));
+
+      const actual2 = a.avg(b, c);
+      assert.deepEqual(jsonExpected, actual2.toJSON());
+      assert.equal(Object.getPrototypeOf(a), Object.getPrototypeOf(actual2));
+    });
 
   });
 
