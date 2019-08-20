ff95e249ecec64e11fb744b9d61d704d2838d41c
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-08-20 00:01:21 -0400

    making it easier to test a variety of training options side by side

--- benchmark/index.js
+++ benchmark/index.js
@@ -8,90 +8,214 @@ shuffle(brainTrainingSet, (x) => JSON.stringify(x.input));
 console.log('Loaded', brainTrainingSet.length, 'training items.');
 console.log(brainTrainingSet[0].input.length, 'Input neurons.');
 
-const multithreadedConfig = {
-  parallel: {threads: 2, partitionSize: 3200, epochs: 10000, log: false, logPeriod: 1, syncMode: false},
-  iterations: 3, // this is passed down to the trainer threads in multithreaded mode
-  learningRate: 0.0001,
-  hiddenLayers: [50, 50],
-  errorThresh: 0.005,
-  activation: 'tanh',
-  momentum: 0.5,
-  binaryThresh: 0.5,
-  beta1: 0.9,
-  beta2: 0.999,
-  epsilon: 1e-8,
-  logPeriod: 1,
-  log: true
-};
+const configs = [
+  {
+    name: 'Single-thread 0.0001 LR',
+    config: {
+      iterations: 10000,
+      learningRate: 0.0001,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
 
-const singlethrededConfig = {
-  iterations: 10000,
-  learningRate: 0.0001,
-  hiddenLayers: [50, 50],
-  errorThresh: 0.005,
-  activation: 'tanh',
-  momentum: 0.5,
-  binaryThresh: 0.5,
-  beta1: 0.9,
-  beta2: 0.999,
-  epsilon: 1e-8,
-  logPeriod: 1,
-  log: true
-};
+  {
+    name: '2 Threads 0.0001 LR Overlapping Partitions',
+    config: {
+      parallel: {threads: 2, partitionSize: 3200, epochs: 10000, log: false, logPeriod: 1, syncMode: false},
+      iterations: 3, // this is passed down to the trainer threads in multithreaded mode
+      learningRate: 0.0001,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
 
-trainSingleThreaded(singlethrededConfig).then(() => trainMultithreaded(multithreadedConfig)).then(() => process.exit());
-//trainMultithreaded(multithreadedConfig).then(() => process.exit());
+  {
+    name: '4 Threads 0.0001 LR Overlapping Partitions',
+    config: {
+      parallel: {threads: 4, partitionSize: 2000, epochs: 10000, log: false, logPeriod: 1, syncMode: false},
+      iterations: 5,
+      learningRate: 0.0001,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
 
-function trainSingleThreaded(config) {
+  {
+    name: 'Single-thread 0.001 LR',
+    config: {
+      iterations: 10000,
+      learningRate: 0.001,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
+
+  {
+    name: '2 Threads 0.001 LR Overlapping Partitions',
+    config: {
+      parallel: {threads: 2, partitionSize: 3200, epochs: 10000, log: false, logPeriod: 1, syncMode: false},
+      iterations: 3,
+      learningRate: 0.001,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
+
+  {
+    name: 'Single-thread 0.01 LR',
+    config: {
+      iterations: 10000,
+      learningRate: 0.01,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
+
+  {
+    name: '2 Threads 0.01 LR Overlapping Partitions',
+    config: {
+      parallel: {threads: 2, partitionSize: 3200, epochs: 10000, log: false, logPeriod: 1, syncMode: false},
+      iterations: 3,
+      learningRate: 0.01,
+      hiddenLayers: [50, 50],
+      errorThresh: 0.005,
+      activation: 'tanh',
+      momentum: 0.5,
+      binaryThresh: 0.5,
+      beta1: 0.9,
+      beta2: 0.999,
+      epsilon: 1e-8,
+      logPeriod: 1,
+      log: true
+    }
+  },
+
+];
+
+let allResults = [];
+let c = 0;
+function processNext() {
+  if (c < configs.length) {
+    runTraining(configs[c++]).then(processNext, fail);
+  } else {
+    printResults();
+    process.exit();
+  }
+}
+processNext();
+
+function runTraining(config) {
+  const name = config.name;
+  console.log('////// STARTING ' + name + ' //////');
+
+  const parallel = config.config.parallel;
+  const net = new brainjs.NeuralNetwork(config.config);
   let itemIterations = 0;
+  let threadCount = 0;
+  const startMs = Date.now();
+
+  let done = function (status) {
+    const endMs = Date.now();
+    const durationS = Math.floor((endMs - startMs) / 1000);
+    const error = status.error;
+    const testError = net.test(brainTrainingSet).error;
+    const itemIterationsPerThread = Math.ceil(itemIterations / threadCount);
+    allResults.push({config, durationS, error, testError, threadCount, itemIterationsPerThread, itemIterations});
+    console.log('////// DONE ' + name + ' //////');
+    console.log(durationS, 'seconds');
+    console.log(itemIterationsPerThread, 'item iterations per thread');
+    console.log(testError, 'testError')
+    console.log(error, 'error');
+  }
+
+  config.config.logPeriod = 1;
+  if (!!parallel) {
+    config.config.log = function (status) {
+      itemIterations = status.itemIterations;
+      threadCount = status.threadCount;
+      console.log('[' + name + '] error =', status.error);
+      console.log('[' + name + '] item iterations =', itemIterations);
+    };
+    return net.trainAsync(brainTrainingSet, config.config).then(done, fail);
+  }
+
+  threadCount = 1;
   const statusRegex = /iterations: (\d+), training error: (\d+(\.\d+)?)/;
-  config.log = (statusStr) => {
-    console.log('Single-threaded progress:' + statusStr);
+  config.config.log = (statusStr) => {
+    console.log('[' + name + '] ' + statusStr);
     const status = statusRegex.exec(statusStr);
-    if (!status) {
-      console.log('Single-threaded status =', statusStr);
-    } else {
+    if (!!status) {
       const iterations = parseInt(status[1]);
       itemIterations += iterations * brainTrainingSet.length;
-      console.log('Single-threaded item iterations =', itemIterations);
+      console.log('[' + name + '] item iterations =', itemIterations);
     }
   };
-
-  console.log('////// Training in single-threaded mode //////');
-  const startMs = Date.now();
-  const net = new brainjs.NeuralNetwork(config);
-  const results = net.train(brainTrainingSet, config);
-  const endMs = Date.now();
-  const testResults = net.test(brainTrainingSet);
-  console.log('Done in', Math.floor((endMs - startMs) / 1000), 'seconds.');
-  console.log('Single-threaded results: ', JSON.stringify(results));
-  console.log('Single-threaded test error =', testResults.error);
-  console.log('Single-threaded item iterations:', itemIterations, 'per thread.');
+  done(net.train(brainTrainingSet, config.config));
   return Promise.resolve();
 }
 
-function trainMultithreaded(config) {
-  let itemIterations = 0;
-  config.log = function (status) {
-    itemIterations = status.itemIterations;
-    console.log('Multi-threaded progress: error =', status.error);
-    console.log('Multi-threaded item iterations =', itemIterations);
-  };
+function fail(reason) {
+  const msg = reason instanceof Error ? reason.message : JSON.stringify(reason);
+  console.error('Benchmark failed: ', msg);
+  process.exit();
+}
 
-  const net = new brainjs.NeuralNetwork(config);
-  
-  console.log('////// Training in multi-threaded mode //////');
-  const startMs = Date.now();
-  return net.trainAsync(brainTrainingSet, config).then((results) => {
-    const endMs = Date.now();
-    console.log('Done in', Math.floor((endMs - startMs) / 1000), 'seconds.');
-    delete results.globalWeights;
-    console.log('Multi-threaded results: ', JSON.stringify(results));
-    const testResults = net.test(brainTrainingSet);
-    console.log('Multi-threaded test error =', testResults.error);
-    console.log('Multi-threaded item iterations:', Math.ceil(itemIterations / results.threadCount), 'per thread.');
-  }, (reason) => {
-    const msg = reason instanceof Error ? reason.message : JSON.stringify(reason);
-    console.error('Multi-threaded training failed: ', msg);
-  });
+function printResults() {
+  console.log('////// Benchmark Results //////')
+  for (let r of allResults) {
+    console.log(r.config.name);
+    console.log('     runtime = ', r.durationS, 'seconds');
+    console.log('     item iterations per thread = ', r.itemIterationsPerThread);
+    console.log('     error = ', r.error);
+    console.log('     test error = ', r.testError);
+  }
 }
