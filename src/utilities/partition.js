8c8d18112d5fec964a7a240beb02616fcc62df52
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-09-15 12:19:10 -0400

    generating duplicate overlapping partitions for multithreaded training data parallelization

--- src/utilities/partition.js
+++ src/utilities/partition.js
@@ -1,4 +1,4 @@
-module.exports = function partition(array, partitions = 1, partitionSize = 1) {
+module.exports = function partition(array, partitions = 1, partitionSize = 1, shuffler = shuffleCopy) {
   if (partitionSize >= array.length) {
     const partitioned = [];
     for (let p = 0; p < partitions; p++) {
@@ -7,6 +7,23 @@ module.exports = function partition(array, partitions = 1, partitionSize = 1) {
     return partitioned;
   }
 
+  const partitioned = [];
+
+  const maxPartitions = (array.length - partitionSize) + 1;
+  while (partitioned.length < partitions) {
+    if (partitioned.length > 0) {
+      array = shuffler(array);
+    }
+
+    const remainingPartitions = partitions - partitioned.length;
+    const newPartitions = Math.min(remainingPartitions, maxPartitions);
+    appendPartitions(array, newPartitions, partitionSize, partitioned);
+  }
+
+  return partitioned;
+};
+
+function appendPartitions(array, partitions, partitionSize, partitioned) {
   const stdSize = Math.floor(array.length / partitions);
   let step = stdSize;
   if (partitionSize <= stdSize) {
@@ -18,7 +35,6 @@ module.exports = function partition(array, partitions = 1, partitionSize = 1) {
     }
   }
   
-  let partitioned = [];
   let from = 0;
   for (let p = 0; p < partitions - 1; p++) {
     partitioned.push(array.slice(from, Math.min(array.length, from + partitionSize)));
@@ -28,4 +44,15 @@ module.exports = function partition(array, partitions = 1, partitionSize = 1) {
   partitioned.push(array.slice(tailIdx));
   
   return partitioned;
+}
+
+function shuffleCopy(array) {
+  const copy = [...array];
+  for (let i = 0; i < copy.length; i++) {
+    const j = Math.floor(Math.random() * copy.length);
+    const swap = copy[i];
+    copy[i] = copy[j];
+    copy[j] = swap;
+  }
+  return copy;
 }
