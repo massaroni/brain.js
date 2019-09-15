9232659359ce28253513b65c59f08d8784561560
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-09-15 14:47:37 -0400

    in practice I usually want overlapping partitions, and all training items must be accounted for

--- __tests__/utilities/partition.js
+++ __tests__/utilities/partition.js
@@ -25,9 +25,9 @@ describe('partition', () => {
     assert.deepStrictEqual(actual, [[1, 2, 3], [2, 3, 4], [4, 5, 6]]);
   });
 
-  it('can partition an odd-len array with overlap and the remainder at the end', () => {
+  it('can partition an odd-length array with some necessary overlap', () => {
     const actual = partition([1, 2, 3, 4, 5], 3, 2);
-    assert.deepStrictEqual(actual, [[1, 2], [3, 4], [4, 5]]);
+    assert.deepStrictEqual(actual, [[1, 2], [2, 3], [4, 5]]);
   });
 
   it('can partition an odd-len array with congruent overlap', () => {
@@ -44,7 +44,7 @@ describe('partition', () => {
     assert.deepStrictEqual(actual, [[1, 2], [3, 4], [5, 6]]);
   });
 
-  it('can fall back to the minimum partition size if the requested partition size by default', () => {
+  it('can fall back to the minimum partition size by default', () => {
     const actual = partition([1, 2, 3, 4, 5, 6], 3);
     assert.deepStrictEqual(actual, [[1, 2], [3, 4], [5, 6]]);
   });
@@ -64,4 +64,38 @@ describe('partition', () => {
     ]);
   });
 
+  it('can partition duplicate overlapping segments with shuffling', () => {
+    const actual = partition(['a','b','c','d'], 4, 3);
+    assert.deepStrictEqual(actual.slice(0, 2), [
+      ['a','b','c'],
+      ['b','c','d']
+    ]);
+    assert.equal(actual.length, 4);
+    assert.equal(actual[2].length, 3);
+    assert.equal(actual[3].length, 3);
+    assert.equal(new Set(actual[2]).size, 3);
+    assert.equal(new Set(actual[3]).size, 3);
+    assert.equal(new Set(actual[2].concat(actual[3])).size, 4);
+  });
+
+  it('can partition larger overlapping segments', () => {
+    const fullSet = arr(0, 100);
+    const actual = partition(fullSet, 6, 40, (a) => a);
+    assert.deepStrictEqual(actual, [
+      arr(0, 40),
+      arr(12, 40),
+      arr(24, 40),
+      arr(36, 40),
+      arr(48, 40),
+      arr(60, 40),
+    ]);
+  });
+
+  function arr(n, length) {
+    let numbers = [];
+    for (let i = 0; i < length; i++) {
+      numbers.push(n + i);
+    }
+    return numbers;
+  }
 });
